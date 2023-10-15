cc547d47d9609ac588d112dc14df0371623beca1
AndresHernandez15 <144160440+AndresHernandez15@users.noreply.github.com>
2023-10-15 12:04:37 -0500

    Update app.py

--- web/app.py
+++ web/app.py
@@ -3,11 +3,10 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-model = []
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return "Hola mundo!"
 
 
 if __name__ == '__main__':
