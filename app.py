253c75923d9f0ec722267d2e60985b4d3004850f
AndresHernandez15 <144160440+AndresHernandez15@users.noreply.github.com>
2023-10-15 14:04:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route("/")
+def home():
+    return "Hola mundo!"
+
+
+if __name__ == '__main__':
+    app.run()
