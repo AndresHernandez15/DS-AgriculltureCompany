c6f3ee183f9674424d46e9b70411cb90ccdbf809
AndresHernandez15 <144160440+AndresHernandez15@users.noreply.github.com>
2023-10-17 02:12:45 -0500

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
 from logic.cropController import CropController
 
 app = Flask(__name__)
@@ -14,9 +15,17 @@ def home():
     return render_template('index.html')
 
 
+""" Crop Endpoints """
+
+
 @app.route("/crop", methods=["GET"])
 def crop():
     return crop_controller.crop()
+
+
+@app.route('/crop_update/<id_crop>', methods=['GET'])
+def crop_update(id_crop):
+    return crop_controller.crop_update(id_crop)
 
 
 @app.route('/crop_detail', methods=['POST'])
@@ -29,14 +38,37 @@ def cultivation():
     return crop_controller.cultivation()
 
 
-@app.route('/crop_update/<id_crop>', methods=['GET'])
-def crop_update(id_crop):
-    return crop_controller.crop_update(id_crop)
-
-
 @app.route('/crop_delete/<id_crop>', methods=['GET'])
 def crop_delete(id_crop):
     return crop_controller.crop_delete(id_crop)
+
+
+""" Harvest Endpoints """
+
+
+@app.route('/harvest/<id_harvest>', methods=['GET'])
+def harvest(id_harvest):
+    return crop_controller.harvest(id_harvest)
+
+
+@app.route('/harvest_update/<id_harvest>', methods=['GET'])
+def harvest_update(id_harvest):
+    return crop_controller.harvest_update(id_harvest)
+
+
+@app.route('/harvest_detail', methods=['POST'])
+def harvest_detail():
+    return crop_controller.harvest_detail()
+
+
+@app.route('/harvested')
+def harvested():
+    return crop_controller.harvested()
+
+
+@app.route('/harvest_delete/<id_harvest>', methods=['GET'])
+def harvest_delete(id_harvest):
+    return crop_controller.harvest_delete(id_harvest)
 
 
 if __name__ == '__main__':
