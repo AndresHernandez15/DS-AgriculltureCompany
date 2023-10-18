9a40ad7345aeff3f8792d11bfdecb252aa20105e
AndresHernandez15 <144160440+AndresHernandez15@users.noreply.github.com>
2023-10-17 23:35:35 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-
 from logic.cropController import CropController
+from logic.SiloController import SiloController
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-model = []
 
 crop_controller = CropController()
-
+silo_controller = SiloController()
 
 @app.route("/")
 def home():
@@ -69,6 +68,61 @@ def harvested():
 @app.route('/harvest_delete/<id_harvest>', methods=['GET'])
 def harvest_delete(id_harvest):
     return crop_controller.harvest_delete(id_harvest)
+
+
+""" Silo Endpoints """
+
+@app.route("/silo", methods=["GET"])
+def silo():
+    return silo_controller.silo()
+
+
+@app.route('/silo_update/<id_silo>', methods=['GET'])
+def silo_update(id_silo):
+    return silo_controller.silo_update(id_silo)
+
+
+@app.route('/silo_detail', methods=['POST'])
+def silo_detail():
+    return silo_controller.silo_detail()
+
+
+@app.route('/silocreation')
+def silocreation():
+    return silo_controller.silocreation()
+
+
+@app.route('/silo_delete/<id_silo>', methods=['GET'])
+def silo_delete(id_silo):
+    return silo_controller.silo_delete(id_silo)
+
+
+""" Product Endpoints """
+
+
+@app.route('/product/<id_product>', methods=['GET'])
+def product(id_product):
+    return silo_controller.product(id_product)
+
+
+@app.route('/product_update/<id_product>', methods=['GET'])
+def product_update(id_product):
+    return silo_controller.product_update(id_product)
+
+
+@app.route('/product_detail', methods=['POST'])
+def product_detail():
+    return silo_controller.product_detail()
+
+
+@app.route('/productcreation')
+def products():
+    return silo_controller.products()
+
+
+@app.route('/product_delete/<id_product>', methods=['GET'])
+def product_delete(id_product):
+    return silo_controller.product_delete(id_product)
 
 
 if __name__ == '__main__':
